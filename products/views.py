f601e17d0d47cbc73227862fce1f809a891afddc
Joao Paulo Genari <jpgenari@gmail.com>
2024-05-12 23:47:14 +0100

    feat: add sorting

--- products/views.py
+++ products/views.py
@@ -10,8 +10,23 @@ def all_products(request):
     
     products = Product.objects.all()
     query = None
+    sort = None
+    direction = None
     
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower name'
+                products = products.annotate(lower_name=Lower('name'))
+                
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+                
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -20,14 +35,16 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
+            
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
+        'current_sorting': current_sorting,
     }
     
     return render(request, 'products/products.html', context)
-
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
