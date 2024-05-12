2ea07536c85bdf4483ab8f99f00d3bcee756159a
Joao Paulo Genari <jpgenari@gmail.com>
2024-05-13 00:17:13 +0100

    feat: add sorting JS and back to top link

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product
 
 # Create your views here.
@@ -18,7 +19,7 @@ def all_products(request):
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
-                sortkey = 'lower name'
+                sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
                 
             if 'direction' in request.GET:
