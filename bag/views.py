d4e66ad8854b481e8fd2ca4122d5a9177f75b663
Joao Paulo Genari <jpgenari@gmail.com>
2024-05-13 21:38:14 +0100

    fix: bug on contexts.py and views.py in bag app causing erros on shopping bag

--- bag/views.py
+++ bag/views.py
@@ -12,7 +12,7 @@ def add_to_bag(request, item_id):
     
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('get', {})
+    bag = request.session.get('bag', {})
     
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
@@ -20,5 +20,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
         
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
