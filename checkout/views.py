ee1af3a2e02b340d6121f1f78ce5824fd2b2327d
Joao Paulo Genari <jpgenari@gmail.com>
2024-05-14 14:47:32 +0100

    feat: add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PGLBfP2MwQn6tAyehjbnfu8idhKZ9j7hWDnoJTNjWKjYxYqKFYqeMyXlFlE2Mwa94nlVjVLcur20DFSGpaPnmjX00MHE9zfs2',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
