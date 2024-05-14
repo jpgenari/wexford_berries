03317e98db90781c7203e1de5c828fa2259fa89e
Joao Paulo Genari <jpgenari@gmail.com>
2024-05-14 14:07:37 +0100

    feat: add checkout views and templates fix: add form.py to properly run the server

--- checkout/models.py
+++ checkout/models.py
@@ -64,8 +64,7 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the order number
-        if it hasn't been set already.
+        Override the original save method to set the lineitem total and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
