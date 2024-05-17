f844f0a903094971bca818c18a549d4bbbefc275
Joao Paulo Genari <jpgenari@gmail.com>
2024-05-17 22:38:43 +0100

    feat: add securering the views

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -8,6 +8,7 @@ from checkout.models import Order
 
 # Create your views here.
 
+@login_required
 def profile(request):
     """Display the user's profile"""
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -33,6 +34,7 @@ def profile(request):
     
     return render(request, template, context)
 
+@login_required
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
     
