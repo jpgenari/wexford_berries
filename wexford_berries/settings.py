40bb057f0a0d9b90c311027962239a9efc466048
Joao Paulo Genari <jpgenari@gmail.com>
2024-05-18 00:28:14 +0100

    setting debug=True to debug bad request (400)

--- wexford_berries/settings.py
+++ wexford_berries/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qac$i6dux4oaixn^3o)m94slbvf44=b2yu_hc!l4=a8tn9gkw8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['wexford-berries.herokuapp.com', 'localhost']
 
