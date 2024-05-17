10c94441c01b6b279cfd953e047592a697639276
Joao Paulo Genari <jpgenari@gmail.com>
2024-05-17 20:33:55 +0100

    feat: admin - add product view, url and template

--- products/forms.py
+++ products/forms.py
@@ -7,7 +7,7 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
         
-    def __init__(self, *args, *kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
         for field_name, field in self.fields.items():
