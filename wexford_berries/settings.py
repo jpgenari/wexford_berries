5164e22c14b06c89da85010330c5452a211bc483
Joao Paulo Genari <jpgenari@gmail.com>
2024-06-28 01:36:48 +0100

    Fix: revert urllib to allow deployment

--- wexford_berries/settings.py
+++ wexford_berries/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['wexford-berries.herokuapp.com', 'wexford-berries-229479cc7505.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['wexford-berries-229479cc7505.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
